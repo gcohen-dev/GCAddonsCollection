0f53bcbef272acf9e50ccb33e40e8a0b165b1477
GuyCohen <gcohen@shutterfly.com>
2020-07-04 21:07:15 +0300

    * Added a Snippet for creating a xib

---
+++ Sources/GCAddonsCollection/Snippets/CreateXibFromSceneDelegate/CreateXibFromSceneSnippet.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Guy Cohen on 01/07/2020.
+//
+
+import Foundation
+
+/**
+ Create Xib From Scene delegate
+ Don't forget to remove main from plist and from the project itself
+ */
+
+//guard let scene = (scene as? UIWindowScene) else { return }
+//let homeView = HomeViewController()
+//let window = UIWindow(windowScene: scene)
+//window.rootViewController = homeView
+//self.window = window
+//window.makeKeyAndVisible()
